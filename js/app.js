5a3ef851467080392f326fdd42b8222dd7ea33af
Sameer Siruguri <siruguri@gmail.com>
2016-03-21 09:46:05 -0700

    [WIP] Draft dropdown behavior

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+window.max_dropdowns = 3;
+
 $(document).ready(function() {
     $('.lesson-square').click(function(evt, target) {
 	$('.course-modal').css('display', 'block');
@@ -8,4 +10,21 @@ $(document).ready(function() {
 	    $('.course-modal').css('display', 'none');
 	}
     });
+
+    $('.selection-dropdown').click(function(evt, target) {
+	tid = $(evt.target).data('target-id');
+	if(typeof(tid) == undefined) {
+	    tid = $(evt.target).closest('.selection-dropdown').data('target-id');
+	}
+	$('#search-dropdown-' + tid).toggle();
+
+	// hide all other dropdowns
+	for(i=1; i<=window.max_dropdowns; i++) {
+	    if(i !== tid) {
+		$('#search-dropdown-' + i).hide();
+	    }
+	}
+		
+    });
+	    
 });
