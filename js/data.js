0a20d68d0c4363e099a0f97d4c0fc4e774b7a9da
Sameer Siruguri <siruguri@gmail.com>
2016-03-31 12:34:16 -0700

    Basically, moved all jQuery to AngularJS.
    * I have now been assimilated.

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 window.dd_choices =[{
     'id': 0,
     'label': "I'm",
-    'options': ['a youth', 'an adult']
+    'options': ['a youth', ['a youth', 'an adult']]
 }, {
     'id': 1,
     'label': "I want to learn",
-    'options': ['basic computer skills', 'entry-level coding', 'about getting jobs']
+    'options': ['basic computer skills', ['basic computer skills', 'entry-level coding', 'about getting jobs']]
 }];
 
--- js/models.js
+++ js/models.js
@@ -1,14 +1,18 @@
 window.max_dropdowns = 3;
 angular.module('techpaths', ['ngRoute']).controller('techpaths_ctrl', function ($scope) {
-    $scope.dd_selections = [0, 0];
     $scope.dd_recs = window.dd_choices;
     $scope.orgs = window.orgs;
+    $scope.close_boxes = [true, true];
+
+    $scope.toggle_boxes = function(f_idx) {
+	$scope.close_boxes[f_idx] = !($scope.close_boxes[f_idx]);
+    }
     
-    $scope.dd_unselected = function(idx) {
+    $scope.select_option = function($event, f_idx, c_idx) {
 	// All options other than the idx'ed option
-	return $scope.dd_recs[idx]['options'].filter(function(elt, i, arr) {
-	    return i != $scope.dd_selections[idx];
-	});
+	$scope.dd_recs[f_idx]['options'][0] = $scope.dd_recs[f_idx]['options'][1][c_idx];
+	$scope.close_boxes[f_idx] = true;
+	$event.stopPropagation();
     };
 
     $scope.dd_rec_search = function(idx, query) {
@@ -19,35 +23,6 @@ angular.module('techpaths', ['ngRoute']).controller('techpaths_ctrl', function (
 	    }
 	});
 	return result;
-    }
-    
-    $(document).ready(function() {
-	$('.option-box').click(function(evt) {
-	    tgt = $(evt.target);
-	    option_index = tgt.data('rec-index');
-	    
-	    result = $scope.dd_rec_search(option_index, tgt.html().trim());
-	    $scope.dd_selections[option_index] = result;
-	    $scope.$digest();
-	    tgt.closest('.search-selection').css('display', 'none');
-	});
-	
-	$('.selection-dropdown').click(function(evt) {
-	    var tid;
-	    tid = $(evt.target).data('target-id');
-	    if(typeof(tid) == 'undefined') {
-		tid = $(evt.target).closest('.selection-dropdown').data('target-id');
-	    }
-	    $('#search-dropdown-' + tid).toggle();
-
-	    // hide all other dropdowns
-	    for(i=0; i < window.max_dropdowns; i++) {
-		if(i !== tid) {
-		    $('#search-dropdown-' + i).hide();
-		}
-	    }
-	});
-    });
-    
+    }       
 });
 
