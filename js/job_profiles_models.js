af9ce5d1b5725b9c02af8d20baa7985e3fec4393
Sameer Siruguri <siruguri@gmail.com>
2016-04-27 09:31:09 -0700

    Add at least one job profile per category

--- js/job_profiles_models.js
+++ js/job_profiles_models.js
@@ -1,5 +1,4 @@
 angular.module('techpaths', ['ngRoute']).controller('jobprofiles_ctrl', ['$scope', function ($scope) {
-    $scope.job_profiles = [{category: 'Product Development', title: 'Product Manager', description: 'Brings a technology product to market in response to a customer need (from vision to planning to execution).'},{category: 'Product Development', title: 'User Experience Designers', description: 'Ensures that users have a valuable experience interacting with a digital or physical product or service.'},{category: 'Software', title: 'Coders/Programmers', description: 'Turns the designs made by software engineers and web developers into data instructions called code that computing devices can follow (end product: software & web pages).'},{category: 'Software', title: 'Web Developer', description: 'Designs and creates websites. Responsible for the look of the site (front-end) and behind the scenes interactions with servers and databases (back-end). Developers who work on both the front-end and back-end are called "full-stack" developers.'}];
     
     $scope.profile_groups = ['Software', ['Software', 'Hardware', 'Product Development', 'Visual Design']]
     
@@ -27,4 +26,15 @@ angular.module('techpaths', ['ngRoute']).controller('jobprofiles_ctrl', ['$scope
     $scope.matches_filter = function(profile) {
 	return profile['category'] === $scope.profile_groups[0];
     };
+
+
+    $scope.job_profiles = [{category: 'Product Development', title: 'Product Manager', description: 'Brings a technology product to market in response to a customer need (from vision to planning to execution).'},
+			   {category: 'Product Development', title: 'User Experience Designers', description: 'Ensures that users have a valuable experience interacting with a digital or physical product or service.'},
+			   {category: 'Software', title: 'Coders/Programmers', description: 'Turns the designs made by software engineers and web developers into data instructions called code that computing devices can follow (end product: software & web pages).'},{category: 'Software', title: 'Web Developer', description: 'Designs and creates websites. Responsible for the look of the site (front-end) and behind the scenes interactions with servers and databases (back-end). Developers who work on both the front-end and back-end are called "full-stack" developers.'},
+			   {category: 'Hardware', title: 'Hardware Engineer', description: 'Designs, develops, and tests physical computing devices and their internal and external components.'},
+			   {category: 'Software', title: 'Software Engineer', description: 'Designs, develops, and tests the virtual operating systems and applications that run on computing devices.'},
+
+			   {category: 'Visual Design', title: 'Graphic & Multi-media Designers (including 3D animators)', description: 'Combines art and technology to visually inspire, inform, and captivate.  Graphic designers work on still images for print or the web, whereas multi-media designers and animators produce animation for video games, movies, television, and the web.'},
+			  ];
+    
 }]);
